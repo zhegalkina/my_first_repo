dc1ef0aa58df821f0dfa33211225eced45f201d5
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-06-06 20:16:49 +0300

    First task

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+answer = input("Enter some digits: ")
+answer_1 = 5 + 3
+answer_2 = 5 - 3
+answer_3 = 5 * 3
+print(answer_1, answer_2, answer_3)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
