11ee26a66ce745d17e484e3cf5a25a54c8ed403f
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-06-06 21:20:57 +0300

    First task

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
-a = int(input("a: "))
-b = int(input("b: "))
-answer_1 = a + b
-answer_2 = a - b
-answer_3 = a * b
-print(answer_1, answer_2, answer_3)
+# a = int(input("a: "))
+# b = int(input("b: "))
+# answer_1 = a + b
+# answer_2 = a - b
+# answer_3 = a * b
+# print(answer_1, answer_2, answer_3)
 
 
+# value = int(input("enter first digit: "))
+# value_2 = int(input("enter second digit: "))
+# # if else
+# if value_2 == 0:
+#     print("Zero division problem!")
+# else:
+#     answer = value / value_2
+#     print(answer)
 
+
+# fingers = int(input("Input number of fingers "))
+# if fingers == 1:
+#     print("Штрафной бросок")
+# elif fingers == 3:
+#     print("Трехочковый бросок")
+# elif fingers == 2:
+#     print("Обычный бросок")
+# else:
+#     print("А в баскетбол ли там играют вообще??")
+
+
+a = int(input("Input digit 1: "))
+b = int(input("Input digit 2: "))
+if a < b:
+    print(a)
+    print(b)
+elif a > b:
+    print(b)
+    print(a)
+else:
+    print("digit 1 = digit 2")
