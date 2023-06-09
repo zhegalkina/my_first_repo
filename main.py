216f815cb064817b70246a3944bead11b8bcef4f
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-06-09 21:52:31 +0300

    First task

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-
-num = range(1, 1000)
-for i in num:
+# list_1 = []
+# for i in range(1,1000):
 #     numb = i ** 2
-#     list1.append(numb)
-# print(list1)
-    if i == 20:
-        num.remove(i)
-print(num)
+#     list_1.append(numb)
+# print(list_1)
+
+
+
+num = list(range(1,1000))
+num_2 = str(num)
+for i in num_2:
+    if i = "20":
+        num_2.remove(i)
+print(num_2)
