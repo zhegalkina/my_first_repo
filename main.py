90d7642829295f3a91a2e8956275bb1ed5d11562
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-06-09 19:32:58 +0300

    First task

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 """"09.06.2023"""
 
 
-digit = int(input("Число n: "))
-for i in range(0, digit+1):
-    if i % 2 == 0:
-        print(i)
+# digit = int(input("Число n: "))
+# for i in range(0, digit+1):
+#     if i % 2 == 0:
+#         print(i)
 
 
 digit = int(input("Число n: "))
 for i in range(0, digit+1):
-    if digit % i == 0:
+    if i == 0:
+        continue
+    elif digit % i == 0:
         print(i)
