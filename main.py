20a479da0cb16655f25e68267e38ffd62517dc03
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-06-09 21:31:46 +0300

    First task

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
-list1 = []
-for i in range(1,1000):
-    numb = i ** 2
-    list1.append(numb)
-print(list1)
+num = range(1, 1000)
+for i in num:
+#     numb = i ** 2
+#     list1.append(numb)
+# print(list1)
+    if i == 20:
+        num.remove(i)
+print(num)
