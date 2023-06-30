5e23dabe5503341d5f3362d4076025c4260cd58b
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-06-30 19:30:59 +0300

    F

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def calendar(year, month, date):
-    print(" it is {} {} {}".format(year, month, date))
-calendar(year = 2023, month = 6, date = " ")
+"""№ 1"""
+
+def calendar(year, month, date = ""):
+    print("it is {} {} {}".format(year, month, date))
+calendar(year = 2023, month = 6)
+
+
+
+"""№ 2"""
+
+def
 
 
