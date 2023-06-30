3c27445fc22b9d49bcb6375d195b0781fe30b444
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-06-30 19:07:21 +0300

    F

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-# list_1 = []
-# for i in range(1,1000):
-#     numb = i ** 2
-#     list_1.append(numb)
-# print(list_1)
-
-
-
-num = list(range(1,1000))
-num_2 = str(num)
-for i in num_2:
-    if i = "20":
-        num_2.remove(i)
-print(num_2)
+def calendar(year, month, date):
+    year = 2023
+    month = june
+    date = (" ")
+    print(calendar)
