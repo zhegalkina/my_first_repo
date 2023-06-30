d8b020026819d1c08d4b4317b2b15bb2b5f0dad9
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-06-30 19:42:04 +0300

    F

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 """№ 1"""
+#
+# def calendar(year, month, date = ""):
+#     print("it is {} {} {}".format(year, month, date))
+# calendar(year = 2023, month = 6, date = 30)
+#
+#
+#
+# """№ 2"""
+#
+# def insect(*args):
+#     print(args)
+# insect('insect1', 'insect2', 'insect3')
 
-def calendar(year, month, date = ""):
-    print("it is {} {} {}".format(year, month, date))
-calendar(year = 2023, month = 6, date = 30)
+"""№ 3"""
 
-
-
-"""№ 2"""
-
-def insect(*args):
-    print(args)
-insect('insect1', 'insect2', 'insect3')
+def car(brand, model = "Q7"):
+    print('{} {}'.format(brand, model))
+car(brand="bmw")
 
